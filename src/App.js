01a47203c1b812b560c47c663c89b559b52a8384
ja2z <93806968+ja2z@users.noreply.github.com>
2022-07-31 15:14:48 -0700

    Consolidate like edges (lines) between nodes. Previously, multiple edges could exist between two given nodes. Now, these edges are consolidated into a single edge. This significantly reduces clutter and makes the Sankey far more usable. Also, tooltip cleanup for better reading of long numbers.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import HighchartsReact from "highcharts-react-official";
 import * as highchartsSankey from "highcharts/modules/sankey";
 //npm install highcharts highcharts-react-official highcharts-sankey
 
-import {
-  client,
-  useConfig,
-  useElementData,
-} from "@sigmacomputing/plugin";
+import { client, useConfig, useElementData } from "@sigmacomputing/plugin";
 
 highchartsSankey(Highcharts);
 
@@ -29,23 +25,64 @@ function App() {
     const measures = config.measures;
 
     // transform sigmaData --> sankey data
-    let dataMap = [];
+    let uniquePair = {};
+    let uniqueParentPair = {};
     if (dimensions && sigmaData?.[dimensions[0]]) {
       for (let i = 0; i < dimensions.length - 1; i++) {
         for (let j = 0; j < sigmaData[dimensions[i]].length; j++) {
+
+          // get parent path of node
+          let parent = "";
+          if (i === 0) {
+            parent = "_root";
+          } else {
+            for (let k = 0; k <= i - 1; k++) {
+              parent = parent + "||" + sigmaData[dimensions[k]][j];
+            }
+          }
+
           const from = sigmaData[dimensions[i]][j];
           const to = sigmaData[dimensions[i + 1]][j];
           const weight = sigmaData[measures[i]][j];
-          const dataPoint = [from, to, weight];
-          dataMap[dataPoint] = dataPoint;
+
+          // dedupe parent rows
+          if (uniqueParentPair[parent] === undefined)
+            uniqueParentPair[parent] = {};
+          if (uniqueParentPair[parent][from] === undefined)
+            uniqueParentPair[parent][from] = {};
+          uniqueParentPair[parent][from][to] = weight;
         }
       }
+
+      // hash agg pairs
+      for (let parent in uniqueParentPair) {
+        for (let from in uniqueParentPair[parent]) {
+          for (let to in uniqueParentPair[parent][from]) {
+            const weight = uniqueParentPair[parent][from][to];
+            if (uniquePair[from] === undefined) uniquePair[from] = {};
+            // Do the actual hash agg
+            // If the from/to pair doesn't exist, create it with the weight
+            // If it already exists in the hash map, add weight to existing weight
+            uniquePair[from][to] =
+              uniquePair[from][to] === undefined
+                ? weight
+                : uniquePair[from][to] + weight;
+          }
+        }
+      }
+
+      // xform agg pairs into array that Sankey chart likes
       let data = [];
       let i = 0;
-      for (var key in dataMap) {
-        data[i] = dataMap[key];
-        i++;
+      for (let from in uniquePair) {
+        for (let to in uniquePair[from]) {
+          const weight = uniquePair[from][to];
+          data[i] = [from, to, weight];
+          i++;
+        }
       }
+
+      // Sankey Highcharts formatting options
       const options = {
         title: {
           text: undefined,
@@ -57,8 +94,12 @@ function App() {
         accessibility: {
           point: {
             valueDescriptionFormat:
-              "{index}. {point.from} to {point.to}, {point.weight}.",
+              "{index} {point.from} to {point.to}, {point.weight}.",
           },
+        },
+        tooltip: {
+          pointFormat: "{point.from} → {point.to}: {point.weight:,.0f}",
+          nodeFormat: "{point.name}: {point.sum:,.0f}",
         },
         series: [
           {
